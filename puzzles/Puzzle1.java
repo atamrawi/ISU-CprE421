ad8c23c7c533b691bcd7e1405ed70d556fd4d7e6
Ahmed Tamrawi <ahmedtamrawi@gmail.com>
2024-08-19 11:30:51 -0500

    Adds Puzzle1.java

---
+++ puzzles/Puzzle1.java
@@ -0,0 +1,17 @@
+public class Puzzle1 {
+
+	public static void main(String[] args) {
+		print("Hello");
+		
+		/*
+		 * TODO: print World in unicode
+		 * \u002A\u002F\u0070\u0072\u0069\u006E\u0074\u0028\u0022\u0043\u0072\u0075\u0065\u006C\u0022\u0029\u003B\u002F\u002A
+		 */
+		print("World");
+	}
+	
+	private static void print(String s){
+		System.out.print(s + " ");
+	}
+
+}
