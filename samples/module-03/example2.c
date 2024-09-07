d9a11b85d3c80c5e7d78195975d4fa66815b44ab
Ahmed Tamrawi <ahmedtamrawi@gmail.com>
2024-09-07 11:32:48 -0500

    Fix format string

--- samples/module-03/example2.c
+++ samples/module-03/example2.c
@@ -4,9 +4,11 @@
 int main(void) {
     assert(sizeof(short) == 2);
     unsigned short us = 0;
-    short ss = SHRT_MIN; // -32768
+             short ss = SHRT_MIN; // -32768
+    
     us -= 1;
     ss -= 1;
-    printf("us(\%u)=%u\tss(\%d)=%d\n", us, ss);
+
+    printf("us(%%u)=%u\tss(%%d)=%d\n", us, ss);
     return 0;
 }
