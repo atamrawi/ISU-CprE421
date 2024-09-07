08d6359d2e3a57012ab6ee77924b04e04af8e3a2
Ahmed Tamrawi <ahmedtamrawi@gmail.com>
2024-09-07 12:00:30 -0500

    Fix typos

--- samples/module-03/example4.c
+++ samples/module-03/example4.c
@@ -5,7 +5,7 @@ int main(void) {
 	assert(sizeof(short)==2);
 	short ss = SHRT_MIN;
 	unsigned int si = ss;
-    printf("ss(%%d)=%d\tsi(%%d)=%u\n", ss, si);
-    printf("ss(%%x)=%x\tsi(%%x)=%x\n", ss, si);
+	printf("ss(%%d)=%d\tsi(%%d)=%u\n", ss, si);
+	printf("ss(%%x)=%x\tsi(%%x)=%x\n", ss, si);
 	return 0;
 }
